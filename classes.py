b3e856c19ac96c584699e03d56c754b824811459
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-16 06:35:43 -0400

    Class or objects in python

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Car:
-    def __init__(self, color: str, horsepower: int ) -> None:
-        self.color = color
+    def __init__(self, brand: str, horsepower: int ) -> None:
+        self.brand = brand
         self.horsepower = horsepower
 
-volvo: Car = Car('red', 230)
-print(volvo.color, volvo.horsepower)
+volvo: Car = Car('Volvo', 230)
+print(volvo.brand, volvo.horsepower)
 
-bmw: Car = Car('black', 400)
-print(bmw.color, bmw.horsepower)
+bmw: Car = Car('BMW', 400)
+print(bmw.brand, bmw.horsepower)
