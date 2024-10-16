9106525683f76f9d6711ee285f23d50e221f8949
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-15 21:18:08 -0400

    data types in python

--- A.py
+++ A.py
@@ -1,9 +1,17 @@
+# integer number
 number: int = 20
+# decimal values are denoted as float
 decimal: float = 12.343
+# set of characters
 text: str = 'hello annei'
+# has two states True or False
 isActive: bool = False
 
+# list of elements, we can add, remove elements
 names: list = ['Bob', 'Anna', 'Luigi']
+# immutable like strings in java
 coordinates: tuple = (1.5, 2.5)
+# cannot have any duplicates
 unique: set = {1, 4, 2, 9}
+# key, value pairs
 data: dict = {'name':'Bob', 'age':12}
