f7f0a45e62806fa61ea8057dd481a39392749b06
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-16 06:42:06 -0400

    Class or objects in python

--- classes.py
+++ classes.py
@@ -3,13 +3,4 @@ class Car:
         self.brand = brand
         self.horsepower = horsepower
 
-    def drive(self) -> None:
-        print(f'{self.brand} is driving!!')
-
-    def get_info(self, size: int) -> None:
-        print(size)
-        print(f'{self.brand} with {self.horsepower} horsepower !!')
-
 volvo: Car = Car('Volvo', 230)
-volvo.get_info(7)
-volvo.drive()
