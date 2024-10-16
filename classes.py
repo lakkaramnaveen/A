0908ce046d1150fcae2d087030bfb6ab121db1e5
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-16 06:48:41 -0400

    Add dunder method

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+
 class Car:
     def __init__(self, brand: str, horsepower: int ) -> None:
         self.brand = brand
@@ -7,7 +10,7 @@ class Car:
     def __str__(self) -> str:
         return f'{self.brand}, {self.horsepower}hp'
 
-    def __add__(self, other) -> str:
+    def __add__(self, other: Self) -> str:
         return f'{self.brand} & {other.brand}'
 
 
