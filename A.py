4b3da7808ba4e283e4205788101679ce9d27b592
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-15 21:08:34 -0400

    data types in python

--- A.py
+++ A.py
@@ -1,3 +1,9 @@
 number: int = 20
 decimal: float = 12.343
 text: str = 'hello annei'
+isActive: bool = False
+
+names: list = ['Bob', 'Anna', 'Luigi']
+coordinates: tuple = (1.5, 2.5)
+unique: set = {1, 4, 2, 9}
+data: dict = {'name':'Bob', 'age':20}
