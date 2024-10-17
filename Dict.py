ab8f2f31e77b0b3420f81fbd8a5915f7c5386776
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-17 14:24:30 -0400

    Dictionary with collision handling

--- Dict.py
+++ Dict.py
@@ -1,8 +1,8 @@
 class HashTable:
     # intialize the variables MAX, arr
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0
@@ -12,19 +12,29 @@ class HashTable:
 
     def __setitem__(self, key, val):
         h = self.get_hash(key)
-        self.arr[h] = val
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element)==2 and element[0]==key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key, val))
 
     def __getitem__(self, key):
         h = self.get_hash(key)
         return self.arr[h]
 
-    def __delitem__(self, key):
-        h = self.get_hash(key)
-        self.arr[h] = None
+    # def __delitem__(self, key):
+    #     h = self.get_hash(key)
+    #     self.arr[h] = None
 
 if __name__ == '__main__':
     t = HashTable()
     t['march 6'] = 120
+    t['march 8'] = 120
+    t['march 9'] = 120
+    t['march 17'] = 459
     print(t['march 6'])
-    del t['march 6']
+    # del t['march 6']
     print(t.arr)
