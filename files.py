82709c06c86a97fc06426c756e788e0c5e334bf3
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-19 09:46:40 -0400

    files operations, functions

--- files.py
+++ files.py
@@ -2,10 +2,7 @@ han = open('A.txt')
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    if line == '':
-        continue
-
-    if len(wds) < 1 or wds[0] == 'help':
+    if len(wds) < 1 or wds[0] != 'help':
         continue
     else:
         print(wds[1])
