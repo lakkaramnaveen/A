335676435978b9cfbe2f6bc43d7b722fc54d5346
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-20 06:54:55 -0400

    binary search updated

--- BinarySearch.py
+++ BinarySearch.py
@@ -2,6 +2,7 @@ searchNumber = int(input('Enter any number to search: '))
 arrayInt = [1, 11, 12, 14, 5, 6, 23, 4, 87]
 arrayInt.sort()
 
+
 def binarySearch(arrayInt, searchNumber):
     f = False
     low = 0
@@ -15,10 +16,6 @@ def binarySearch(arrayInt, searchNumber):
         else:
             high = mid - 1
     return f
-
-
-
-
 
 
 print(binarySearch(arrayInt,searchNumber))
