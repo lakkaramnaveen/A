16ab5f32f9c8974faad01935687e2cc0c4bf720d
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-21 09:46:58 -0400

    updated tuple

--- graph.py
+++ graph.py
@@ -1,10 +1,22 @@
 class Graph:
     def __init__(self, edges) -> None:
         self.edges = edges
+        self.graph_dict = {}
+        for start, end in self.edges:
+
 
 if __name__ == '__main__':
     # Tuple
     routes = [
         ("Mumbai","Paris"),
-
+        ("Newyork","France"),
+        ("Newyork","California"),
+        ("Hyderabad","Dallas"),
+        ("Banglore","Seattle"),
     ]
+
+    route_graph = Graph(routes)
+    d = {
+        "Mumbai": ["Paris", "Dubai"],
+        "Paris": ["Dubai", "New York"]
+    }
