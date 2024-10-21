c97f931dafacb2960db10facf43bf3b3290bc9c3
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-21 11:20:09 -0400

    binary search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,6 +1,3 @@
-arr = [1, 12, 42, 21, 34, 21]
-arr.sort()
-
 def binarySearch(a, element):
     mid = low = 0
     high = len(a)
@@ -32,8 +29,19 @@ def linearSearch(a, element):
             return True
     return False
 
+def allOccurences(a, element):
+    list1 = []
+    for i in range(len(a)-1):
+        if a[i] == element:
+            list1.append(i)
+    return list1
 
 if __name__ == '__main__':
-    print(binarySearch(arr, 13))
-    print(linearSearch(arr, 12))
-    print(recursiveBinarySearch(arr, 12322, 0, len(arr)-1))
+    arr = [1, 12, 42, 21, 34, 21]
+    numbers = [1,4,6,9,10,5,7]
+    # numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+    numbers.sort()
+    print(binarySearch(numbers, 5))
+    print(linearSearch(numbers, 5))
+    print(recursiveBinarySearch(numbers, 5, 0, len(numbers)-1))
+    print(allOccurences(numbers, 10))
