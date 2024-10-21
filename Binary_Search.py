f102e2c06ca0b793acfbf8985c80bd79fb08236c
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-21 11:04:27 -0400

    recursive binary search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,28 +1,39 @@
 arr = [1, 12, 42, 21, 34, 21]
 arr.sort()
 
-class Nani:
-    def binarySearch(self, a, element):
-        mid = low = 0
-        high = len(a)
-        while(low<=high):
-            mid = int( (low + high)/2 )
-            if arr[mid] == element:
-                return True
-            elif arr[mid] < element:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return False
+def binarySearch(a, element):
+    mid = low = 0
+    high = len(a)
+    while(low<=high):
+        mid = int( (low + high)/2 )
+        if arr[mid] == element:
+            return True
+        elif arr[mid] < element:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
 
-    def linearSearch(self, a, element):
-        for i in a:
-            if i == element:
-                return True
-        return False
+def recursiveBinarySearch(a, element, low, high):
+    if high < low:
+        return -1
+    mid = (low + high)//2
+    if arr[mid] == element:
+        return mid
+    elif arr[mid] < element:
+        low = mid + 1
+    else:
+        high = mid - 1
+    return recursiveBinarySearch(a, element, low, high)
+
+def linearSearch(a, element):
+    for i in a:
+        if i == element:
+            return True
+    return False
 
 
 if __name__ == '__main__':
-    bin = Nani()
-    print(bin.binarySearch(arr, 13))
-    print(bin.linearSearch(arr, 12))
+    print(binarySearch(arr, 13))
+    print(linearSearch(arr, 12))
+    print(recursiveBinarySearch(arr, 12322, 0, len(arr)-1))
