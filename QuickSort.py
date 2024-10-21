c7dea2bae45448ed08c6d499acb6fb2fbb603472
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-21 18:31:04 -0400

    quick sort implementation

--- QuickSort.py
+++ QuickSort.py
@@ -1,7 +1,34 @@
-def quick_sort(elements):
+def swap(a, b, arr):
+    if a!=b:
+        tmp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = tmp
 
+
+def partition(elements, start, end):
+    pivot_index = 0
+    pivot = elements[pivot_index]
+
+    while start < end:
+        while elements[start] <= pivot:
+            start += 1
+
+        while elements[end] > pivot:
+            end -= 1
+
+    if start < end:
+        swap(start,end,elements)
+    swap(pivot_index, end, elements)
+
+    return end
+
+def quick_sort(elements, start, end):
+    if start < end:
+        pi = partition(elements, start, end)
+
+    partition(elements)
 
 if __name__ == '__main__':
     elements = [5,4,5,12,3,1,24,56,77,86,3,783,32]
-    quick_sort(elements)
+    quick_sort(elements, 0, len(elements)-1)
     print(elements)
