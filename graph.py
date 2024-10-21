2c0fd7ab317bfd9032b50a0cb49d5111e97369cd
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-21 10:25:01 -0400

    shortest path

--- graph.py
+++ graph.py
@@ -29,8 +29,19 @@ class Graph:
         return paths
 
     def get_shortest_path(self, start, end, path=[]):
-
-
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in self.graph_dict:
+            return None
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp :
+                    if shortest_path is None or len(sp)< len(shortest_path):
+                        shortest_path = sp
+        return shortest_path
 
 
 if __name__ == '__main__':
@@ -49,3 +60,4 @@ if __name__ == '__main__':
     end="California"
 
     print(f"Paths between {start} and {end}: ", route_graph.get_paths(start, end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start, end))
